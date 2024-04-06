b03c806560f48b28d793fa1720d8b17f35a25f43
Davi-Porto <16davisantoss@gmail.com>
2024-04-06 02:21:05 -0300

    Finish

--- main.py
+++ main.py
@@ -28,19 +28,88 @@ def bTD(b):
                 d+=bits[i]
         return d
 
-try:
-    ip = list(map(lambda n: int(n), input("IP (x.x.x.x): ").split(".")))
-except:
-    print("Ocorreu algum erro com endereço ip inserido")
-    err=True
+# Logic AND between two bits, return 0 or 1 based on result
+def andB(a, b):
+    a, b = int(a), int(b)
+    return "1" if a==1 and b==1 else "0"
+
+# Join list elements using a separator, return new string
+def join(l, sep=""):
+    result=""
+    l=list(map(lambda n: str(n), l))
+    for i, item in enumerate(l):
+        result+=item
+        result+="" if i==len(l)-1 else sep
+    return result
 
 
-try:
-    mask = list(map(lambda n: int(n), input("Mask (x.x.x.x): ").split(".")))
-except:
-    print("Ocorreu algum erro com a máscara de ip inserida")
-    err=True
+def c32oct(str):
+    aux=[0, 8, 16, 24, 32]
+    l=[]
+    print()
+    for i, v in enumerate(aux):
+        if i>0:
+            l.append(bTD(str[aux[i-1]:v]))
+    return join(l, ".")
 
+# Main-line of code
+def main():
+    try:
+        # # Tests (Comment if not testing)
+        # ip = [192, 186, 2, 3]
+        # mask = [255, 255, 255, 0]
+        
+        ip = list(map(lambda n: int(n), input("IP (x.x.x.x): ").split(".")))
+        mask = list(map(lambda n: int(n), input("Mask (x.x.x.x): ").split(".")))
+        
+        # Subnet:
+        
+        # Ip to binary
+        # Mask to binary
+        # Logical AND with them
+        # Subnet to decimal
+        
+        # IP to binary:
+        ipBinary = "".join(list(map(dTB, ip)))
+        
+        # Mask to binary
+        maskBinary = "".join(list(map(dTB, mask)))
+        
+        # Logical AND with them
+        srBin=""
+        for i in range(32):
+            srBin+=andB(ipBinary[i], maskBinary[i])
+        
+        # Subnet to decimal
+        subnet=c32oct(srBin)
+        
+        
+        # Broadcast:
+        
+        # Subnet to binary
+        # Mask to binary, invert it
+        # Logical sum with them chars
+        # Broadcast to decimal
+        
+        # Subnet to binary:
+        # Already exists in srBin
+        
+        # Mask to binary, invert it
+        # Just invert it:
+        maskBinaryInverted = join(list(map(lambda c: "1" if c=="0" else "0", maskBinary)))
+        
+        # Logical sum with them chars:
+        broadcastBin=""
+        for i, c in enumerate(maskBinaryInverted):
+            broadcastBin+=str(int(c)+int(srBin[i]))
+        
+        # Broadcast to decimal
+        broadcast = c32oct(broadcastBin)
+        
+        
+        print("Subnet:",subnet)
+        print("Broadcast:", broadcast)
+    except:
+        print("\nOcorreu algum erro com os valores fornecidos")
 
-if not err:
-    print("Prosseguir")
+main()
