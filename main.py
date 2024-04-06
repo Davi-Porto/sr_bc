d9ffc989bba4d2bebfb8d8855beccdfc27b5dcb2
Davi-Porto <16davisantoss@gmail.com>
2024-04-06 04:52:58 -0300

    optimize code, reduce lines and bug fix

--- main.py
+++ main.py
@@ -1,62 +1,53 @@
 # Add RegEx to verify IP and Mask
 # from re import *
 
-err=False
-
 # Convert  decimal number into a string witch contains 8 bits, eight zeros and one, return it
 def dTB(d):
     d = int(d) if type(d)==type("") else d
     if type(d)==type(0):
-        bits=[128,64,32,16,8,4,2,1]
         aux=d
-        bin=["0","0","0","0","0","0","0","0"]
-        for i, bit in enumerate(bits):
-            if aux>=bit:
-                bin[i]="1"
-                aux-=bit
+        bin=[]
+        for i in range(7,-1,-1):
+            if aux>=2**i:
+                bin.append("1")
+                aux-=2**i
             else:
-                bin[i]="0"
+                bin.append("0")
         return "".join(bin)
 
 # Convert binary into a decimal one, return it
 def bTD(b):
     if type(b)==type("") and len(b)==8:
-        bits=[128,64,32,16,8,4,2,1]
         d=0
         for i, c in enumerate(b):
             if c=="1":
-                d+=bits[i]
+                d+=2**(7-i)
         return d
 
 # Logic AND between two bits, return 0 or 1 based on result
 def andB(a, b):
-    a, b = int(a), int(b)
-    return "1" if a==1 and b==1 else "0"
+    return str(int(a) and int(b))
 
 # Join list elements using a separator, return new string
 def join(l, sep=""):
     result=""
     l=list(map(lambda n: str(n), l))
     for i, item in enumerate(l):
-        result+=item
-        result+="" if i==len(l)-1 else sep
+        result+=item+sep if not i==len(l)-1 else item
     return result
 
 
 def c32oct(str):
-    aux=[0, 8, 16, 24, 32]
     l=[]
-    print()
-    for i, v in enumerate(aux):
-        if i>0:
-            l.append(bTD(str[aux[i-1]:v]))
+    for v in range(1,5):
+        l.append(bTD(str[(v-1)*8:v*8]))
     return join(l, ".")
 
 # Main-line of code
 def main():
     try:
         # # Tests (Comment if not testing)
-        # ip = [192, 186, 2, 3]
+        # ip = [192, 168, 0, 4]
         # mask = [255, 255, 255, 0]
         
         ip = list(map(lambda n: int(n), input("IP (x.x.x.x): ").split(".")))
@@ -110,6 +101,6 @@ def main():
         print("Subnet:",subnet)
         print("Broadcast:", broadcast)
     except:
-        print("\nOcorreu algum erro com os valores fornecidos")
+        print("\nAn erro has occurred with this values")
 
 main()
